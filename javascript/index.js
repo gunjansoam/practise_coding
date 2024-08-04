d61afa448ccb150e7588ec454b3184e88213a402
gunjansoam <gsoam3626@gmail.com>
2024-08-04 15:19:29 +0530

    arrays

--- javascript/index.js
+++ javascript/index.js
@@ -1,2 +1,68 @@
+// let a = null;
+// let b = 354;
+// let c = true;
+// let d = BigInt("567") + BigInt("3")
+// let e = "Gunjan soam"
+// let f = Symbol("symbol")
+// let g
+// console.log(a,b,c,d,e,f,g)
+// console.log(typeof c)
 
 
+// const item = {
+//     "Gunjan soam":true,
+//     "sourav": false,
+//     "aaku":67,
+//     "ram":undefined
+// }
+// console.log(item["sourav"])
+
+// question >> wap  to store name phone number and marks of a student using objects.
+// const collegian = {
+//      name: "Gunjan Soam",
+//      mobile : "+91 32615653",
+// }
+// console.log(collegian)
+
+// question  create a variable of type string and try to add a number to it.
+// let a = "gunjan soam";
+// let b = 95
+// console.log(a+b)
+// console.log(typeof(a+b))
+
+// console.log()
+  
+
+// const a = {
+//     name : "gunjan",
+//     section : "cs",
+//     ishod: false
+
+// }
+
+// console.log(a)
+  
+//operators in js 
+// console.log("operators in js")
+// let a = 45;
+// let b = 5;
+// airthmetic operators
+// console.log("a+b=",a+b)
+// console.log("a-b=",a-b)
+// console.log("a*b=",a*b)
+// console.log("a/b=",a/b)
+// console.log("a%b=",a%b)
+// console.log("a**b=",a**b)
+// console.log("a++=",a++)
+// console.log("a--=",a--)
+// console.log(a)
+// console.log("++a=",++a)
+// console.log(a)
+// console.log("--a=",--a)
+// console.log(a)
+let comp1 = 6;
+let comp2 = "7";
+console.log("comp1==comp2 is ",comp1 == comp2)
+console.log("comp1!=comp2 is ",comp1!=comp2)
+console.log("comp1 === comp2",comp1===comp2)
+console.log("comp1!==comp1",comp1!==comp2)
