3db9faa83a67d1c743f63ef8f9b527117555f464
gunjansoam <gsoam3626@gmail.com>
2024-08-06 17:01:26 +0530

    array qs

--- javascript/array.js
+++ javascript/array.js
@@ -8,10 +8,60 @@
 
 // note: array are mutable
 // Looping over an array
+// for loop
+// let names = ["Gunjan","megha","simran","cia"]
+// for(let idx= 0; idx<names.length; idx++){
+//     console.log(names[idx]);
+// }
 
-let names = ["Gunjan","megha","simran","cia"]
-for(let idx= 0; idx<names.length; idx++){
-    console.log(names[idx]);
-}
+// for of
+// for(let item of names) {
+//     console.log(item);
+// }
+//  let cities = ["delhi","pune","mumbai","hyderabad","gurgoan"];
+//  for(let city of cities){
+//     console.log(city.toUpperCase());
+//     console.log(city);
+//  }
+ 
+
+//qs for a given array marks of student ->[85,97,44,37,76,60]
+// find the average marks of entire class.
+// let marks = [85,97,44,37,76,60];
+// let sum = 0;
+// for(let val of marks){
+//     sum += val;
+//     console.log(val);
+// }
+// let avg = sum/marks.length;
+// console.log(`avg marks of class = ${avg}`);
+
+
+// Qs for a given array with prices of 5 items -> [250,645,300,900,50] all items have an offer of 10% Off on them .chnage the array to store final price after applying offer.
+
+// let thing = [250,645,300,900,50];
+// let idx = 0;
+// console.log("value of thing=",thing);
+
+// for (let val of thing){
+//     console.log(`value of index ${idx}=${val}`);
+//     let offer = val/10;
+//    thing[idx] = thing[idx]-offer;
+//     console.log(`value after offer=${thing[idx]}`);
+//     idx++
+// }
+
+
+// second method
+// let items = [250,645,300,900,50];
+// for(let i = 0; i< items.length;i++){
+//     let offer = items[i]/10;
+//     items[i] -= offer; 
+// }
+// console.log(items);
+
+
+
+//arrays in js
 
 
