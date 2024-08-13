f58b0e191972822b611d8caacfb2060d55d79b01
gunjansoam <gsoam3626@gmail.com>
2024-08-13 11:55:57 +0530

    js qs

--- javascript/question.js
+++ javascript/question.js
@@ -1,3 +1,51 @@
-console.log("hello world!");
-alert("Hello world!");
-document.write(<h1>"hello world"</h1>);
+// console.log("hello world!");
+// alert("Hello world!");
+// document.write("hello world");
+
+//Qs 1 add two numbers 
+// static number
+// let a = 5;
+// let b = 6;
+// let c = a+b;
+// console.log("the sum of a+b = ",c);
+// console.log(`the sum of ${a}+${b}= ${c}`);
+
+
+//dynamic number
+// let a = prompt("enter first number = ","");
+// let b = prompt("enter second number = ","");
+// let c = parseInt(a)+parseInt(b);
+// console.log(`the sum of number is = ${c}`);
+
+
+// Qs squareroot of a number
+// let a = prompt("enter a number","");
+// let b = Math.sqrt(a);
+// console.log(b);
+
+
+// Qs area of  right angle triangle
+
+// let base = prompt("enter the base value");
+// let height = prompt("enter the  height value");
+// let area = (base)*(height)/2;
+
+// console.log(area);
+
+// find the area of regular triangle with herons formula
+// let a = parseFloat(prompt('enter the number'));
+// let b = parseFloat(prompt("enter second value"));
+// let c = parseFloat(prompt("enter the thrid value"));
+// let s = (a+b+c)/2;
+
+// let ar = s*(s-a)*(s-b)*(s-c);
+// console.log(ar);
+// let area = Math.sqrt(ar);
+// console.log("area of triangle is",area);
+
+
+
+
+
+
+
